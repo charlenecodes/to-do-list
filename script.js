fd487050a45f764a486ebcb2178f076b643154b6
lava-git <105036972+lava-git@users.noreply.github.com>
2022-12-13 19:17:39 +0100

    removed comments, final version

--- script.js
+++ script.js
@@ -7,25 +7,19 @@ let empty = document.querySelector("#empty-task")
 let li = document.createElement("li");
 
 function addTask() { 
+    let li = document.createElement("li");
     li.classList.add("li-style")
     li.innerText = input.value;
     toDo.appendChild(li) /* the order of lines 9-12 are very important, otherwise this will not be functioning correctly */
+    li.addEventListener("click", function () {
+        li.style.textDecoration = "line-through";
+    })
+    li.addEventListener("dblclick", function () {
+        toDo.removeChild(li);
+    })
     input.value = ""; /* clears the input field after pressing enter */
     empty.classList = "hide"
 }
 
 
 addButton.addEventListener("click", addTask)
-
-// create functions in order to remove unwanted tasks and mark completed tasks
-
-function remove() {
-    toDo.removeChild(li);
-}
-
-function completed() {
-    li.style.textDecoration = "line-through";
-}
-
-li.addEventListener("dblclick", remove)
-li.addEventListener("click", completed)
